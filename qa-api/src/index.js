42ce7af86c7c68f6cef8bd624e1b913471096cb6
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-08-29 22:30:57 -0400

    Add API routes to server

--- qa-api/src/index.js
+++ qa-api/src/index.js
@@ -24,6 +24,50 @@ app.use(cors());
 // Log HTTP requests
 app.use(logger("combined"));
 
+// Route to GET all questions
+app.get("/", (req, res) => {
+    const qs = questions.map(q => ({
+        id: q.id,
+        title: q.title,
+        description: q.description,
+        answers: q.answers.length
+    }));
+    res.send(qs);
+});
+
+// Route to GET a specific question
+app.get("/:id", (req, res) => {
+    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+    if (question.length > 1) return res.status(500).send();
+    if (question.length === 0) return res.status(404).send();
+    res.send(question[0]);
+});
+
+// Route to POST a new question
+app.post("/", (req, res) => {
+    const {title, description} = req.body;
+    const newQuestion = {
+        id: questions.length + 1,
+        title,
+        description,
+        answers: []
+    };
+    questions.push(newQuestion);
+    res.status(200).send();
+});
+
+// Route to POST a new answer to a question
+app.post("/answer/:id", (req, res) => {
+    const {answer} = req.body;
+    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+    if (question.length > 1) return res.status(500).send();
+    if (question.length === 0) return res.status(404).send();
+    question[0].answers.push({
+        answer
+    });
+    res.status(200).send();
+});
+
 // Start the API server
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
