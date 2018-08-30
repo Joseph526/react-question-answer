42488de123e1ee7887fbb27157653761549bfd07
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-08-29 23:50:20 -0400

    Create NavBar component

--- qa-react/src/App.js
+++ qa-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <NavBar />
           <p>Work in progress.</p>
         </div>
       </Router>
